09bd3f0219c9e4f79777008a8a996052f8ef5382
Joshua Farmer <rhubarbjosh@outlook.com>
2025-02-10 14:49:58 +0000

    tests

--- make.sh
+++ make.sh
@@ -3,16 +3,16 @@ gcc -g "src/main.c" -o "bin/jcc-i386" -DARCH_I386_JDECL
 
 for file in tests/*.c; do
         base="${file%.c}"
-        
+
         ./bin/jcc-i386-cdecl "$file" -o "${base}_c.s"
         nasm "${base}_c.s" -o "${base}_c.o" -felf32
         gcc "${base}_c.o" -o "${base}_c" -m32
         "./${base}_c"
-        echo "CDECL RETURN CODE: $?"
+        echo "$file CDECL RETURN CODE: $?"
         
         ./bin/jcc-i386 "$file" -o "${base}_j.s"
         nasm "${base}_j.s" -o "${base}_j.o" -felf32
         ld -m elf_i386 -s -o "${base}_j" "${base}_j.o"
         "./${base}_j"
-        echo "JDECL RETURN CODE: $?"
+        echo "$file JDECL RETURN CODE: $?"
 done
