6ad087f57c9c910e3a00bacd22142058429cc295
Joshua Farmer <rhubarbjosh@outlook.com>
2025-02-10 14:49:04 +0000

    tests

--- make.sh
+++ make.sh
@@ -1,18 +1,18 @@
 gcc -g "src/main.c" -o "bin/jcc-i386-cdecl" -DARCH_I386_CDECL
 gcc -g "src/main.c" -o "bin/jcc-i386" -DARCH_I386_JDECL
 
-./bin/jcc-i386-cdecl tests/1.c -o tests/1_c.s
-if [ $? -eq 0 ]; then
-        nasm tests/1_c.s -o tests/1_c.o -felf32
-        gcc tests/1_c.o -o tests/1_c -m32
-        ./tests/1_c
-        echo 'CDECL RETURN CODE:' $?
-fi
-
-./bin/jcc-i386 tests/1.c -o tests/1_j.s
-if [ $? -eq 0 ]; then
-        nasm tests/1_j.s -o tests/1_j.o -felf32
-        ld -m elf_i386 -s -o tests/1_j tests/1_j.o
-        ./tests/1_j
-        echo 'JDECL RETURN CODE:' $?
-fi
+for file in tests/*.c; do
+        base="${file%.c}"
+        
+        ./bin/jcc-i386-cdecl "$file" -o "${base}_c.s"
+        nasm "${base}_c.s" -o "${base}_c.o" -felf32
+        gcc "${base}_c.o" -o "${base}_c" -m32
+        "./${base}_c"
+        echo "CDECL RETURN CODE: $?"
+        
+        ./bin/jcc-i386 "$file" -o "${base}_j.s"
+        nasm "${base}_j.s" -o "${base}_j.o" -felf32
+        ld -m elf_i386 -s -o "${base}_j" "${base}_j.o"
+        "./${base}_j"
+        echo "JDECL RETURN CODE: $?"
+done
