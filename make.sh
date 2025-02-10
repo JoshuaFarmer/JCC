f043f846a1b71449896c77110a69d690b9504ec1
Joshua Farmer <rhubarbjosh@outlook.com>
2025-02-10 14:59:09 +0000

    the

--- make.sh
+++ make.sh
@@ -1,18 +1,20 @@
-gcc -g "src/main.c" -o "bin/jcc-i386-cdecl" -DARCH_I386_CDECL
-gcc -g "src/main.c" -o "bin/jcc-i386" -DARCH_I386_JDECL
+#!/bin/bash
+mkdir -p tests/o tests/s tests/src tests/exe
 
-for file in tests/*.c; do
-        base="${file%.c}"
+for file in tests/src/*.c; do
+    base=$(basename "$file" .c)
 
-        ./bin/jcc-i386-cdecl "$file" -o "${base}_c.s"
-        nasm "${base}_c.s" -o "${base}_c.o" -felf32
-        gcc "${base}_c.o" -o "${base}_c" -m32
-        "./${base}_c"
-        echo "$file CDECL RETURN CODE: $?"
-        
-        ./bin/jcc-i386 "$file" -o "${base}_j.s"
-        nasm "${base}_j.s" -o "${base}_j.o" -felf32
-        ld -m elf_i386 -s -o "${base}_j" "${base}_j.o"
-        "./${base}_j"
-        echo "$file JDECL RETURN CODE: $?"
+    # CDECL Compilation
+    ./bin/jcc-i386-cdecl "$file" -o "tests/s/${base}_c.s"
+    nasm "tests/s/${base}_c.s" -o "tests/o/${base}_c.o" -felf32
+    gcc "tests/o/${base}_c.o" -o "tests/exe/${base}_c" -m32
+    "./tests/exe/${base}_c"
+    echo "CDECL RETURN CODE: $?"
+
+    # JDECL Compilation
+    ./bin/jcc-i386 "$file" -o "tests/s/${base}_j.s"
+    nasm "tests/s/${base}_j.s" -o "tests/o/${base}_j.o" -felf32
+    ld -m elf_i386 -s -o "tests/exe/${base}_j" "tests/o/${base}_j.o"
+    "./tests/exe/${base}_j"
+    echo "JDECL RETURN CODE: $?"
 done
