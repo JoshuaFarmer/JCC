64caadd26ff719a8ef25713d05928622f2cd1b77
Joshua Farmer <rhubarbjosh@outlook.com>
2025-02-13 19:56:40 +0000

    the

--- make.sh
+++ make.sh
@@ -3,4 +3,4 @@ gcc "src/main.c" -o "bin/jcc-i386" -DARCH_I386 -DCALL_JDECL
 gcc "src/main.c" -o "bin/jcc-i85" -DARCH_I8085
 gcc "src/main.c" -o "bin/jcc-i8086" -DARCH_I8086
 sh lines.sh
-sh test.sh
+#sh test.sh
